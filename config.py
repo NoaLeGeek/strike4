2b129c80eb523bed99c514b5dae65b9653d4b263
NoaLeGeek <noacraft68@gmail.com>
2024-07-22 15:53:53 +0200

    game.py and some things

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ class Config:
     def __init__(self) -> None:
         self.width = ctypes.windll.user32.GetSystemMetrics(0)
         self.height = ctypes.windll.user32.GetSystemMetrics(1)
+        self.margin = 16
         self.rows = 5
         self.columns = 5
         self.length_win = 4
+        self.tile_size = (self.height - 2 * self.margin) // self.rows
         self.debug = False
